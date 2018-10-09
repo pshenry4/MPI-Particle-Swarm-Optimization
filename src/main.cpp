34e2ba628ffc2c8935913a851049619ddf85020b
Rob Green <greenr@bgsu.edu>
2018-10-09 12:22:18 -0400

    Working code and .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -9,39 +9,18 @@ double f(double x){
     return retVal;
 }
 
-double serialTrap(double a, double b, double n){
+double serialTrap(double a, double b, int n){
 
-    double h, retValue, x;
+    double h, retValue;
 
     h = (b-a)/n;
     retValue = (f(a) + f(b))/2.0;
+
+    #pragma omp parallel for reduction(+: retValue) 
     for(int i=1; i<=n-1; i++){
-        x = a + i*h;
-        retValue += f(x);
+        retValue += f(a + i*h);
     }
     retValue = h*retValue;
-
-    return retValue;
-}
-
-double parallelTrap(double a, double b, double n){
-    double h, x, retValue;
-    int myId = omp_get_thread_num();
-    int numThreads = omp_get_num_threads();
-
-    int myN, myA, myB;
-
-    h = (b-a)/n; 
-    myN = n/numThreads;  
-    myA = a + myId * myN * h; 
-    myB = myA + myN * h; 
-    retValue = (f(myA) + f(myB))/2.0; 
-
-    for (int i = 1; i < myN; i++) {
-        x = myA + i*h;
-        retValue += f(x);
-    }
-    retValue = retValue * h; 
 
     return retValue;
 }
@@ -53,27 +32,16 @@ int main(){
     
     a = 0, b = 16, n = 16;
 
+    result = 0.0;
+    numThreads = 1;
+    omp_set_num_threads(numThreads);
     result = serialTrap(a, b, n);
     std::cout << "Serial Result: " << result << std::endl;
 
     result = 0.0;
-    omp_set_num_threads(4);
-
-    #pragma omp parallel num_threads(numThreads) 
-    {
-
-        //Incorrect First option
-        // #pragma omp critical
-        // result += parallelTrap(a, b, n);
-
-        double localResult;
-        localResult += parallelTrap(a, b, n);
-
-        #pragma omp critical
-        result += localResult;
-   }
-
-
+    numThreads = 4;
+    omp_set_num_threads(numThreads);
+    result = serialTrap(a, b, n);
     std::cout << "Parallel Result: " << result << std::endl;
 
     return 0;
