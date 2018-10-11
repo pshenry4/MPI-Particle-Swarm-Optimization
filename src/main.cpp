e08b112c4796e8a13e05624b700d4f96e41ccb8f
Dr. Green <greenr@bgsu.edu>
2018-10-11 11:29:26 -0400

    Working Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <omp.h>
 #include <iostream>
+#include "CStopWatch.h"
 
 double f(double x){
     double retVal = 0.0;
@@ -9,7 +10,7 @@ double f(double x){
     return retVal;
 }
 
-double serialTrap(double a, double b, int n){
+double trap(double a, double b, int n){
 
     double h, retValue;
 
@@ -28,11 +29,29 @@ double serialTrap(double a, double b, int n){
 int main(){
 
     int numThreads = 4, a, b, n;
+    int threadMin, threadMax, threadStep;
+    int nMin, nMax, nStep;
+    CStopWatch timer;
     double result;
-    
-    a = 0, b = 16, n = 16;
 
-    result = 0.0;
+    a = 0, b = 16;
+
+    nMin = 1000; nMax = 10001; nStep=1000;
+    threadMin = 1; threadMax = 5; threadStep = 1;
+
+    for(numThreads=threadMin; numThreads<threadMax; numThreads++){
+        for(n=nMin; n<nMax; n+=nStep){
+            result = 0.0;
+            omp_set_num_threads(numThreads);
+            timer.startTimer();
+            result = trap(a, b, n); 
+            timer.stopTimer();
+            std::cout << numThreads << ", " << n << ", " << result << ", " << timer.getElapsedTime() << "\n";
+        }
+    }
+
+    /*
+ * result = 0.0;
     numThreads = 1;
     omp_set_num_threads(numThreads);
     result = serialTrap(a, b, n);
@@ -43,6 +62,6 @@ int main(){
     omp_set_num_threads(numThreads);
     result = serialTrap(a, b, n);
     std::cout << "Parallel Result: " << result << std::endl;
-
+*/
     return 0;
 }
