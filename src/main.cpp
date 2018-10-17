badd78152f5913835939525136a7d4c21dc45aba
Rob Green <greenr@bgsu.edu>
2018-10-17 16:07:00 -0400

    Working Serial Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <omp.h>
 #include <iostream>
 #include "CStopWatch.h"
+#include <vector>
+#include <unistd.h>
+#include <random>
 
-void serialCountSort(std::vector<int>& a){
+std::random_device rd;                         // only used once to initialise (seed) engine
+std::mt19937 rng(rd());                        // random-number engine used (Mersenne-Twister in this case)
+std::uniform_int_distribution<int> uni(0, 2000); // guaranteed unbiased
+
+std::vector<int> generateList(int n) {
+
+    std::vector<int> retValue;
+    srandom(time(NULL));
+    for(int i = 0; i < n; i++){
+      retValue.push_back(uni(rng));
+    }
+
+    return retValue;
+} 
+
+void countSort(std::vector<int>& a){
 
     std::vector<int> temp;
-    int n;
+    int n, count;
 
     n = a.size();
     temp.resize(n);
@@ -17,6 +35,7 @@ void serialCountSort(std::vector<int>& a){
             }else if(a[j] == a[i] && j < i){
                 count++;
             }
+        }
         temp[count] = a[i];
     }
 
@@ -26,21 +45,25 @@ void serialCountSort(std::vector<int>& a){
 int main(){
 
     int threadMin, threadMax, threadStep;
-    int numTrials;
+    int numTrials, n;
     CStopWatch timer;
+    std::vector<int> result;
 
+    threadMin = 1; threadMax = 12; threadStep = 1;
+    numTrials = 11;
+    n = 10000;
 
-    threadMin = 1; threadMax = 10; threadStep = 1;
-    numTrials = 10;
-
-    for(int numThreads=threadMin; numThreads<=threadMax; numThreads++){
+    for(int numThreads=threadMin; numThreads<=threadMax; numThreads+=threadStep){
         for(int curTrial=0; curTrial<numTrials; curTrial++){
             
             omp_set_num_threads(numThreads);
+            result = generateList(n);
             timer.startTimer();
             // Call functions here
+            countSort(result);
             timer.stopTimer();
-            std::cout << numThreads << ", " << /** Add Results Output Here **/ << ", " << timer.getElapsedTime() << "\n";
+            std::cout << numThreads << ", " << timer.getElapsedTime() << "\n";
+            result.clear();
         }
     }
     
